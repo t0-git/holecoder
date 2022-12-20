97c03b5ce1310a9f762a4a4930a42735561866f8
t0-git <52962854+t0-git@users.noreply.github.com>
2022-12-20 12:54:14 +0100

    Adding output file to ingest easily in tools.

--- holecoder.py
+++ holecoder.py
@@ -6,11 +6,19 @@ import logging
 import html
 import string
 
+def stdout_or_not(payload, boolean, output=None, encoding=None):
+    if boolean:
+        logging.info(f"{encoding}:")
+        logging.info(f"{payload}\r\n")
+    else:
+        output.write(f"{payload}\r\n")
+
+
 def encoder(args):
 
     # Change this to your need, \r\n or \n    
     linebreak = "\r\n"
-
+    
     if args.f:
         with open(args.f, 'r') as lines:
             payload = lines.read()
@@ -18,55 +26,54 @@ def encoder(args):
     else:
         payload = args.p
 
+    if args.o:
+        output = open(args.o, 'a')
+        boolean = False
+    else:
+        boolean = True
+        output = None
+    
+
     # Payload
-    logging.info("Payload:")
-    logging.info(f"{payload}\r\n")
+    stdout_or_not(payload, boolean, output, "Payload") 
     
     # Partial URL encoding
-    logging.info("Partial URL encoding:")
-    logging.info(f"{urllib.parse.quote(payload)}\r\n")
+    stdout_or_not(urllib.parse.quote(payload), boolean, output, "Partial URL encoding")
     
     # Partial URL encoding using + as space
-    logging.info("Partial URL encoding using + as space:")
-    logging.info(f"{urllib.parse.quote_plus(payload)}\r\n")
+    stdout_or_not(urllib.parse.quote_plus(payload), boolean, output, "Partial URL encoding using + as space")
     
     # Full URL encoding
     percent_encode = ""
-    logging.info("Full URL encoding:")
     for i in payload:
         hexa = i.encode('latin-1').hex()
         percent_encode += f"%{hexa}"
-    logging.info(f"{percent_encode}\r\n")
+    stdout_or_not(percent_encode, boolean, output, "Full URL encoding")
     
     # Partial double URL encoding
-    logging.info("Partial double URL encoding:")
-    logging.info(f"{urllib.parse.quote(urllib.parse.quote(payload))}\r\n")
+    stdout_or_not(urllib.parse.quote(urllib.parse.quote(payload)), boolean, output, "Partial double URL encoding")
     
     # Full double URL encoding
     percent_encode = ""
-    logging.info("Full double URL encoding:")
     for i in payload:
         hexa = i.encode('latin-1').hex()
         percent_encode += f"%{hexa}"
-    logging.info(f"{urllib.parse.quote(percent_encode)}\r\n")
-
+    stdout_or_not(urllib.parse.quote(urllib.parse.quote(percent_encode)), boolean, output, "Full double URL encoding")
+    
     # Partial HTML entities
-    logging.info("Partial HTML entities:")
     entities = html.escape(payload, quote=True)
-    logging.info(f"{entities}\r\n")
+    stdout_or_not(entities, boolean, output, "Partial HTML entities")
     
     # Full HTML entities
-    logging.info("Full HTML entities:")
     # Check if the linebreak is \r\n, if so, change to \n, as \r is not supported in HTML entities
     if linebreak == "\r\n" and args.f:
         linebreak = "\n"
-        logging.info("\\r\\n has been replaced by \\n")  
         with open(args.f, 'r') as lines:
             html_payload = lines.read()
             html_payload = linebreak.join(html_payload.splitlines())
     else:
         html_payload = payload
-
+    
     inverted_entities = html.entities.html5
     entities = {v: k for k, v in inverted_entities.items()}
     encoded = ""
@@ -75,7 +82,7 @@ def encoder(args):
             encoded += f'&{entities[char]}'
         else:
             encoded += char
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Full HTML entities")
     
     # Partial HTML decimal encoding
     encoded = ''
@@ -84,15 +91,13 @@ def encoder(args):
             encoded += f'&#{ord(char)};'
         else:
             encoded += char
-    logging.info("Partial HTML entities decimal encoding:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Partial HTML entities decimal encoding")
     
     # Full HTML decimal encoding
     encoded = ''
     for char in payload:
         encoded += f'&#{ord(char)};'
-    logging.info("Full HTML entities decimal encoding:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Full HTML entities decimal encoding")
     
     # Partial HTML decimal encoding with five zeros
     encoded = ''
@@ -101,15 +106,13 @@ def encoder(args):
             encoded += f'&#00000{ord(char)};'
         else:
             encoded += char
-    logging.info("Partial HTML entities decimal encoding with five zeros:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Partial HTML entities decimal encoding with five zeros")
     
     # Full HTML decimal encoding with five zeros
     encoded = ''
     for char in payload:
         encoded += f'&#00000{ord(char)};'
-    logging.info("Full HTML entities decimal encoding with five zeros:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Full HTML entities decimal encoding with five zeros")
     
     # Partial HTML decimal encoding with ten zeros
     encoded = ''
@@ -118,16 +121,14 @@ def encoder(args):
             encoded += f'&#0000000000{ord(char)};'
         else:
             encoded += char
-    logging.info("Partial HTML entities decimal encoding with ten zeros:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Partial HTML entities decimal encoding with ten zeros")
     
     
     # Full HTML decimal encoding with ten zeros
     encoded = ''
     for char in payload:
         encoded += f'&#0000000000{ord(char)};'
-    logging.info("Full HTML entities decimal encoding with ten zeros:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Full HTML entities decimal encoding with ten zeros")
     
     
     # Partial HTML hexadecimal encoding
@@ -138,8 +139,7 @@ def encoder(args):
             encoded += f'&#x{hexa};'
         else:
             encoded += char
-    logging.info("Partial HTML entities hexadecimal encoding:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Partial HTML entities hexadecimal encoding")
     
     
     # Full HTML hexadecimal encoding
@@ -147,61 +147,53 @@ def encoder(args):
     for char in payload:
         hexa = char.encode('latin-1').hex()
         encoded += f'&#x{hexa};'
-    logging.info("Full HTML entities hexadecimal encoding:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Full HTML entities hexadecimal encoding")
     
     
     # Unicode encoding
     encoded = ''
     for char in payload:
         encoded += '\\u00{0:x}'.format(ord(char))
-    logging.info("Unicode:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Unicode")
     
     # ECMAScript6 encoding
     encoded = ''
     for char in payload:
         encoded += '\\u' + "{" + '{0:x}'.format(ord(char)) + "}"
-    logging.info("ES6:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "ES6")
     
     # ECMAScript6 encoding with five zeros
     encoded = ''
     for char in payload:
         encoded += '\\u' + "{00000" + '{0:x}'.format(ord(char)) + "}"
-    logging.info("ES6 with five zeros:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "ES6 with five zeros")
     
     
     # ECMAScript6 encoding with ten zeros
     encoded = ''
     for char in payload:
         encoded += '\\u' + "{0000000000" + '{0:x}'.format(ord(char)) + "}"
-    logging.info("ES6 with ten zeros:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "ES6 with ten zeros")
     
     # Hex escaping
     hex_escape = ''
     for i in payload:
         hexa = i.encode('latin-1').hex()
         hex_escape += '\\x' + hexa
-    logging.info("Hex escaping:")
-    logging.info(f"{hex_escape}\r\n")
+    stdout_or_not(hex_escape, boolean, output, "Hex escaping")
     
     # Octal escaping
     encoded = ''
     for char in payload:
         encoded += '\\{0:o}'.format(ord(char))
-    logging.info("Octal escaping:")
-    logging.info(f"{encoded}\r\n")
+    stdout_or_not(encoded, boolean, output, "Octal escaping")
     
     
     # SQL Char in decimal
     encoded = ''
     for char in payload:
         encoded += 'CHAR({})+'.format(ord(char))
-    logging.info("SQL Char in decimal:")
-    logging.info(f"{encoded[:-1]}\r\n")
+    stdout_or_not(encoded[:-1], boolean, output, "SQL char in decimal")
     
     
     
@@ -209,8 +201,7 @@ def encoder(args):
     encoded = ''
     for char in payload:
         encoded += 'CHAR(x{0:x})+'.format(ord(char))
-    logging.info("SQL char in hex:")
-    logging.info(f"{encoded[:-1]}\r\n")
+    stdout_or_not(encoded[:-1], boolean, output, "SQL char in hex")
 
 
 
@@ -219,6 +210,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", help="Payload to encode")
     parser.add_argument("-f", help="Data inside a file to encode")
+    parser.add_argument("-o", help="File to output payloads (easier to ingest in intruder for example")
     args = parser.parse_args()
     if args.p is None and args.f is None:
         parser.error("At least one of -p (payload) or -f (file) is required")
